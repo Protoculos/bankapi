e071f7e93d02303bbc2008ee15717f3cdc5d30be
Protoculos <mazdak2012@gmail.com>
2022-11-24 17:07:12 +0300

    topic2 lesson3 json Encoder

--- main.go
+++ main.go
@@ -1,18 +1,37 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
 )
 
+type Customer struct {
+	Name    string `json:"name,omitempty"`
+	City    string `json:"city,omitempty"`
+	Zipcode string `json:"zipcode,omitempty"`
+}
+
 func main() {
 	//define routes
 	http.HandleFunc("/greet", greet)
+	http.HandleFunc("/customers", getAllCustomers)
+
+	fmt.Println("Started server on 7000 port")
 	//starting server
 	log.Fatal(http.ListenAndServe(":7000", nil))
+
 }
 
 func greet(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprint(w, "Hello World!")
+}
+func getAllCustomers(w http.ResponseWriter, r *http.Request) {
+	customers := []Customer{
+		{"John", "Novorossiysk", "112345"},
+		{"Less", "Moscow", "983940"},
+	}
+	w.Header().Add("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(customers)
 }
